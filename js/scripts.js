8ae1058ad9f1137fc1aed0f743500bb8813fb01e
Emily Onishi <emilyonishi93@gmail.com>
2023-07-07 11:48:44 -0400

    add gallons 2 liters converter

--- js/scripts.js
+++ js/scripts.js
@@ -22,19 +22,27 @@ function tbspToTsp(input) {
 }
 
 function cupToTbsp(input) {
-    return input * 16.2307
+    return input * 16.2307;
 }
 
 function cupToMl(input) {
     return tspToMl(tbspToTsp(cupToTbsp(input))).toFixed(4);
 }
 
+function galToLiter(input) {
+    return input * 3.8;
+}
+
 
 //interface
-const weight = window.prompt("how many pounds do u have?")
+const weight = window.prompt("how many pounds do u have?");
 
 window.alert("ur " + weight + " pound(s) is " + lbToOz(weight) + " ounces which is equal to " + lbToGrams(weight).toFixed(2) + " grams.");
 
-const measure = window.prompt("How many cups do u have o.o")
+const measure = window.prompt("How many cups do u have o.o");
 
-window.alert("ur " + measure + " cup(s) is " + cupToTbsp(measure) + " tablespoons which is also " + tbspToTsp(cupToTbsp(measure)).toFixed(4) + " teaspoons n that is also " + cupToMl(measure) + " milliliters.");
+window.alert("ur " + measure + " cup(s) is " + cupToTbsp(measure).toFixed(4) + " tablespoons which is also " + tbspToTsp(cupToTbsp(measure)).toFixed(4) + " teaspoons n that is also " + cupToMl(measure) + " milliliters.");
+
+const volume = window.prompt("how many gallons do u have?");
+
+window.alert("ur " + volume + " gallons is equal to " + galToLiter(volume).toFixed(2) + " liters.");
