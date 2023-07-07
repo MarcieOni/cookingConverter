7ec88c2bea7fa403503f8e8bd84b21d030171eea
Emily Onishi <emilyonishi93@gmail.com>
2023-07-07 11:34:42 -0400

    add functions to convert cups to tbsp to tsp to milliliter

--- js/scripts.js
+++ js/scripts.js
@@ -12,6 +12,29 @@ function lbToGrams(input) {
 }
 
 
+function tspToMl(input) {
+    const milliliter = 4.93;
+    return (input * milliliter);
+}
+
+function tbspToTsp(input) {
+    return input * 3;
+}
+
+function cupToTbsp(input) {
+    return input * 16.2307
+}
+
+function cupToMl(input) {
+    return tspToMl(tbspToTsp(cupToTbsp(input))).toFixed(4);
+}
+
+
+//interface
 const weight = window.prompt("how many pounds do u have?")
 
-window.alert("ur " + weight + " pound(s) is " + lbToOz(weight) + " ounces which is equal to " + lbToGrams(weight).toFixed(2) + " grams.")
+window.alert("ur " + weight + " pound(s) is " + lbToOz(weight) + " ounces which is equal to " + lbToGrams(weight).toFixed(2) + " grams.");
+
+const measure = window.prompt("How many cups do u have o.o")
+
+window.alert("ur " + measure + "cup(s) is " + cupToTbsp(measure) + "tablespoons which is also " + tbspToTsp(cupToTbsp(measure)) + "teaspoons n that is also " + cupToMl(measure) + "milliliters.");
